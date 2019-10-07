41a65ac3a94123f39d09ef6f38f0eb321a88c5e0
Saurabh Saxena <48159530+saurabh69912162@users.noreply.github.com>
2019-10-07 12:26:18 +0530

    commit

--- base/customusermodel/settings.py
+++ base/customusermodel/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+SITE_ID = 1
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- base/customusermodel/urls.py
+++ base/customusermodel/urls.py
@@ -1,21 +1,18 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from accounts.views import register, login_view, profile, \
-    change_password, edit_profile,edit_business,userlist,edit_creator, \
-    edit_me
+from accounts.views import *
 
 
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path, include
 
-
 urlpatterns = [
-
+    path('admin/', admin.site.urls),
     url(r'^profile/$', profile, name='profile'),
-    url(r'^admin/', admin.site.urls),
     url(r'^register/$', register),
     path('', login_view),
+    path('logout/', logout_view),
     url(r'^password/$', change_password, name='change_password'),
     url(r'^profile/edit$', edit_profile),
     #url(r'^profile/business$', edit_business),
@@ -23,6 +20,7 @@ urlpatterns = [
     url(r'^profile/details', edit_me),
     url(r'^api/v1/', userlist.as_view()),
 ]
+
 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
